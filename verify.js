3283bde6114b74eb5adf8755e84485a731aa435b
emoueitchaien <manojpradhan628@gmail.com>
2020-12-08 17:14:30 +0545

    linked script file and formatted code

---
+++ verify.js
@@ -0,0 +1,17 @@
+const answerarray = ["b", "c", "a", "b"];
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let score = 0;
+  const usersans = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+  usersans.forEach((value, index) => {
+    if (value === answerarray[index]) {
+      score += 25;
+    }
+  });
+
+  console.log(score);
+});
